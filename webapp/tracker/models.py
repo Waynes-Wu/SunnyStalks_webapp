49a49987008e42e670b6101c768747f3707926e2
Waynes-Wu <waynessantos@gmail.com>
2023-11-05 03:43:24 +0800

    changes to database

--- webapp/tracker/models.py
+++ webapp/tracker/models.py
@@ -4,24 +4,30 @@ from django.contrib.auth.models import AbstractUser
 class User(AbstractUser):
     pass
 
-class Branch(models.Model):
-    address = models.CharField(max_length=255)
-    costs = models.DecimalField(max_digits=10, decimal_places=2)
-    def getCosts():
-        # compute average from purchases
-        return;
-
 class GroceryStore(models.Model):
     name = models.CharField(max_length=255)
-    branch = models.ForeignKey(Branch, on_delete=models.CASCADE, related_name='stores')
+
+class Branch(models.Model):
+    address = models.CharField(max_length=255)
+    grocery_store = models.ForeignKey(GroceryStore, on_delete=models.CASCADE, related_name='branches')
+    items = models.ManyToManyField('Item', related_name='branches')
+
+    def getCosts(self):
+        return self.purchases.all().mean()
+
 
 class Item(models.Model):
     name = models.CharField(max_length=255)
     brand = models.CharField(max_length=255)
-    grocery_store = models.ForeignKey(GroceryStore, on_delete=models.CASCADE, related_name='items')
-    price = models.DecimalField(max_digits=10, decimal_places=2)
+    weight = models.DecimalField(max_digits=10, decimal_places=2)
 
-class Purchase(models.Model):
-    item = models.ForeignKey(Item, on_delete=models.CASCADE, related_name='purchases')
-    grocery_store = models.ForeignKey(GroceryStore, on_delete=models.CASCADE, related_name='purchases')
+class PurchaseHistory(models.Model):
     date = models.DateField(auto_now_add=True)
+    travel_expense = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)
+    nickname = models.CharField(max_length=255, null=True, blank=True)
+    grocery_store = models.ForeignKey(Branch, on_delete=models.CASCADE, related_name='purchases')
+
+class PurchaseItems(models.Model):
+    purchase = models.ForeignKey(PurchaseHistory, on_delete=models.CASCADE, related_name='itemsPurchased')
+    item = models.ForeignKey(Item, on_delete=models.CASCADE)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
